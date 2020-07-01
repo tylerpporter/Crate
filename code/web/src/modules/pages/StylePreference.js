f924890393d7ba96938ec3920a305d42adf22ae7
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-07-01 15:32:01 -0600

    Add StylePreference Component with annotation

---
+++ code/web/src/modules/pages/StylePreference.js
@@ -0,0 +1,9 @@
+/* Where the Component rendering of StylePreference informtion will be displayed */
+/* import Paths of correct files we need to connect to React, Redux, UI, etc.. */
+
+/* Imports */
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import { connect } from 'react-redux'
+// import { Helmet } from 'react-helmet'
+// import { Link } from 'react-router-dom'
