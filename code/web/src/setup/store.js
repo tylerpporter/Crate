a8c1b06b37e8f3f7c056bcfc6c089b315cdd3333
“Lauren <lucerolaurenm@gmail.com>
2020-07-05 14:08:17 -0400

    Add Redux dev tools

--- code/web/src/setup/store.js
+++ code/web/src/setup/store.js
@@ -1,14 +1,15 @@
 // Imports
-import { compose, combineReducers } from 'redux'
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import { compose, combineReducers } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 // App Imports
-import common from '../modules/common/api/state'
-import user from '../modules/user/api/state'
-import * as product from '../modules/product/api/state'
-import * as subscription from '../modules/subscription/api/state'
-import * as crate from '../modules/crate/api/state'
+import common from "../modules/common/api/state";
+import user from "../modules/user/api/state";
+import * as product from "../modules/product/api/state";
+import * as subscription from "../modules/subscription/api/state";
+import * as crate from "../modules/crate/api/state";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 // App Reducer
 const appReducer = combineReducers({
@@ -16,23 +17,23 @@ const appReducer = combineReducers({
   user,
   ...product,
   ...subscription,
-  ...crate
-})
+  ...crate,
+});
 
 // Root Reducer
 export const rootReducer = (state, action) => {
-  if (action.type === 'RESET') {
-    state = undefined
+  if (action.type === "RESET") {
+    state = undefined;
   }
 
-  return appReducer(state, action)
-}
+  return appReducer(state, action);
+};
 
 // Load initial state from server side
-let initialState
-if (typeof window !== 'undefined') {
-  initialState = window.__INITIAL_STATE__
-  delete window.__INITIAL_STATE__
+let initialState;
+if (typeof window !== "undefined") {
+  initialState = window.__INITIAL_STATE__;
+  delete window.__INITIAL_STATE__;
 }
 
 // Store
@@ -40,7 +41,5 @@ export const store = createStore(
   rootReducer,
   initialState,
 
-  compose(
-    applyMiddleware(thunk),
-  )
-)
+  compose(composeWithDevTools(applyMiddleware(thunk)))
+);
