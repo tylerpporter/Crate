1325ad6f4aa91a4344adb51edbf42ba9b64e92ac
Tim Nguyen <tim.nguyen2112@gmail.com>
2020-07-11 15:29:35 -0600

    Add jest testing library to package.json and create intial test

--- code/web/src/modules/user/test/StylePreferences.test.js
+++ code/web/src/modules/user/test/StylePreferences.test.js
@@ -6,3 +6,70 @@ import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, rootReducer, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
+
+describe("StylePreference", () => {
+  let user;
+  let router;
+  let store;
+  let mockUpdateStylePreference;
+  beforeEach(() => {
+  mockUpdateStylePreference = jest.fn()
+    user = () => {
+      return {
+        error: null,
+        isLoading: false,
+        isAuthenticated: true,
+        details: {
+            id: 1,
+            name:"Sarah",
+            email:"sarah@crate.com",
+            role: "USER",
+            stylePreference: null,
+          }
+        }
+    }
+    const appReducer = combineReducers({
+      user,
+      // logout
+    })
+  //   // Root Reducer
+    const rootReducer = (state, action) => {
+      if (action.type === 'UPDATE_STYLE') {
+        state = action.details.stylePreference
+      }
+      return appReducer(state, action)
+    }
+    // Load initial state from server side
+    // let initialState =  Object.assign(user())
+    let initialState
+        if (typeof window !== 'undefined') {
+            initialState = window.__INITIAL_STATE__
+            delete window.__INITIAL_STATE__
+        }
+    // store = createStore(rootReducer, initialState)
+    store = createStore(rootReducer, applyMiddleware(thunk))
+    // store.dispatch = jest.fn()
+    router =
+    <Provider store={store}>
+        {/* <BrowserRouter> */}
+        <MemoryRouter>
+          <StylePreferences/>
+        </MemoryRouter>
+        {/* </BrowserRouter> */}
+    </Provider>
+    })
+
+    it("Should render the correct Labels", () => {
+      const { getByText } = render(router)
+
+      const dressesTitleElement = getByText("Dresses");
+      const topsTitleElement = getByText("Tops");
+      const bottomsTitleElement = getByText("Bottoms");
+      const shoesTitleElement = getByText("Shoes");
+    
+      expect(dressesTitleElement).toBeInTheDocument();
+      expect(topsTitleElement).toBeInTheDocument();
+      expect(bottomsTitleElement).toBeInTheDocument();
+      expect(shoesTitleElement).toBeInTheDocument();
+    })
+  })
